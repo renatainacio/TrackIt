fe307eb08ca4783cc6d79dfdf94824e559d6caa9
Renata Inacio <revazgauska@gmail.com>
2023-06-05 15:23:19 -0300

    calendar onClick detail

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -15,6 +15,9 @@ export default function HistoryPage() {
     const[history, setHistory] = useState([]);
     const [green, setGreen] = useState([]);
     const [red, setRed] = useState([]);
+    const[showDetails, setShowDetails] = useState(false);
+    const[daySelected, setDaySelected] = useState({});
+    const[date, setDate] = useState("");
     const config = {
         headers: {
             "Authorization": `Bearer ${user.token}`
@@ -45,25 +48,34 @@ export default function HistoryPage() {
             });
     } , []);
 
-    function isSameDay(a, b) {
-        console.log(a === b);
-        return a === b;
-      }
-
     function calendarStyle(date) {
-        // console.log(green);
-        // console.log(red);
-        // console.log(date.date);
-
         if(green.find((dDate) => dDate.getTime() === date.date.getTime()))
-        {
             return 'highlight-green';
-        }
         else if (red.find((dDate) => dDate.getTime() === date.date.getTime()))
-        {
             return 'highlight-red';
-        }
     };
+
+    function clickDay(value) {
+        let day;
+        let month;
+        if(value.getDate() < 10) {
+            day = `0${value.getDate()}`;
+        }
+        else {
+            day = value.getDate().toString();
+        }
+        if(value.getMonth() < 10) {
+            month = `0${value.getMonth() + 1}`;
+        }
+        else {
+            month = (value.getMonth() + 1).toString();
+        }
+        let d = `${day}/${month}/${value.getFullYear()}`;
+        setDate(`${day}/${month}/${value.getFullYear()}`);
+        setDaySelected(history.filter((day) => day.day === d));
+        console.log(history.filter((day) => day.day === d));
+        setShowDetails(true);
+    }
 
     return(
         <>
@@ -71,8 +83,16 @@ export default function HistoryPage() {
             <SCContent>
                 <h1>Histórico</h1>
                 <SCHistory>
-                    <Calendar calendarType="US" tileClassName={calendarStyle} />
+                    <Calendar calendarType="US" tileClassName={calendarStyle} onClickDay={clickDay} />
                 </SCHistory>
+                {showDetails ? <h2>{date}</h2> : ""}
+                {showDetails && ((daySelected[0] && daySelected[0].length === 0) || !(daySelected[0])) ? <p>Nenhum hábito para este dia!</p> : ""}
+                {showDetails && daySelected[0] && daySelected[0].length != 0 ? daySelected[0].habits.map(habit => 
+                    <SCHabitDetail done={habit.done}>
+                        {habit.done ? <ion-icon name="checkmark-circle-outline"></ion-icon> : <ion-icon name="close-circle-outline" done={habit.done}></ion-icon>}
+                        <p key={habit.id}>{habit.name}</p>
+                    </SCHabitDetail>
+                    ) : ""}
             </SCContent>
             <Menu />
         </>
@@ -81,6 +101,7 @@ export default function HistoryPage() {
 
 const SCHistory = styled.div`
     margin-top: 14px;
+    margin-bottom: 14px;
     display: flex;
     background-color: #FFFFFF;
     border-radius: 10px;
@@ -113,4 +134,14 @@ const SCHistory = styled.div`
             border-radius: 100%;
         }
     }
+`
+const SCHabitDetail = styled.div`
+    display: flex;
+    p {
+        margin-left: 4px;
+        color: ${props=>props.done === true ? "green" : "red"};
+    }
+    ion-icon {
+        color: ${props=>props.done === true ? "green" : "red"};
+    } 
 `
